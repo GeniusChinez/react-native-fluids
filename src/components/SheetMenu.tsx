026d6d0e4ccaa2ea50e4d8e760b00ff01379f09e
Genius Chinembiri <geniuschinembiri@gmail.com>
2024-02-03 06:34:20 +0200

    feat: add blank sheets

--- src/components/SheetMenu.tsx
+++ src/components/SheetMenu.tsx
@@ -1,12 +1,9 @@
-/* eslint-disable react-native/no-inline-styles */
-import { useTheme } from 'theme-native';
 import React, { useMemo, type RefObject } from 'react';
 import { MenuLayout, type MenuLayoutProps } from './MenuLayout';
-import { BottomSheet, type BottomSheetRef } from 'react-native-sheet';
+import { type BottomSheetRef } from 'react-native-sheet';
 import { useRef } from 'react';
-import { BottomSheetsProvider } from './BottomSheetsProvider';
-import { ScrollView } from 'react-native';
 import { View } from './View';
+import { BlankSheet } from './BlankSheet';
 
 export interface SheetMenuProps extends MenuLayoutProps {
   height?: number;
@@ -15,40 +12,15 @@ export interface SheetMenuProps extends MenuLayoutProps {
 
 export function SheetMenu(props: SheetMenuProps) {
   const { height = 200, sheetRef, ...menuProps } = props;
-
-  const theme = useTheme();
-  const { isDarkMode } = theme;
-
   const bottomSheetRef = useRef<BottomSheetRef>(null);
 
   const ref = useMemo(() => sheetRef || bottomSheetRef, [sheetRef]);
 
   return (
-    <BottomSheet
-      height={height || 200}
-      ref={ref}
-      sheetStyle={{
-        padding: theme.spacing[2],
-        backgroundColor: isDarkMode
-          ? theme.color.Stone[800]
-          : theme.color.White,
-      }}
-      backdropClosesSheet
-    >
-      <BottomSheetsProvider customRef={ref}>
-        <ScrollView
-          style={{
-            flex: 1,
-          }}
-          contentContainerStyle={{
-            paddingBottom: theme.spacing[12],
-          }}
-        >
-          <View grows>
-            <MenuLayout {...menuProps} />
-          </View>
-        </ScrollView>
-      </BottomSheetsProvider>
-    </BottomSheet>
+    <BlankSheet height={height || 200} sheetRef={ref}>
+      <View grows>
+        <MenuLayout {...menuProps} />
+      </View>
+    </BlankSheet>
   );
 }
