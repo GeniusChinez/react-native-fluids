bf87ba14c10736a96eb205c1f5a53207b70700ad
Genius Chinembiri <geniuschinembiri@gmail.com>
2024-02-06 04:04:58 +0200

    feat: add scrollable screen content

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, { type PropsWithChildren } from 'react';
 import { BottomAppbar, type BottomAppbarProps } from './BottomAppbar';
 import {
@@ -8,11 +9,13 @@ import { TopAppbar, type TopAppbarProps } from './TopAppbar';
 import { useKeyboard } from '../hooks/useKeyboard';
 import { View, type ViewProps } from './View';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native';
 
 export interface ScreenProps extends PropsWithChildren<ViewProps> {
   topAppbar?: TopAppbarProps;
   bottomNavigation?: BottomNavigationProps;
   bottomAppbar?: BottomAppbarProps;
+  isScrollable?: boolean;
 }
 
 export function Screen(props: ScreenProps) {
@@ -21,6 +24,7 @@ export function Screen(props: ScreenProps) {
     bottomNavigation,
     topAppbar,
     children,
+    isScrollable,
     ...restOfProps
   } = props;
   const keyboard = useKeyboard();
@@ -30,18 +34,40 @@ export function Screen(props: ScreenProps) {
   return (
     <>
       {!!topAppbar && <TopAppbar {...topAppbar} />}
-      <View
-        bg="white"
-        darkBg="black"
-        grows
-        {...restOfProps}
-        style={{
-          paddingTop: topAppbar ? undefined : insets.top,
-          ...(restOfProps.style as any),
-        }}
-      >
-        {children}
-      </View>
+      {isScrollable && (
+        <ScrollView
+          contentContainerStyle={{
+            flexGrow: 1,
+          }}
+        >
+          <View
+            bg="white"
+            darkBg="black"
+            grows
+            {...restOfProps}
+            style={{
+              paddingTop: topAppbar ? undefined : insets.top,
+              ...(restOfProps.style as any),
+            }}
+          >
+            {children}
+          </View>
+        </ScrollView>
+      )}
+      {!isScrollable && (
+        <View
+          bg="white"
+          darkBg="black"
+          grows
+          {...restOfProps}
+          style={{
+            paddingTop: topAppbar ? undefined : insets.top,
+            ...(restOfProps.style as any),
+          }}
+        >
+          {children}
+        </View>
+      )}
       {!!bottomAppbar && !keyboard.shown && <BottomAppbar {...bottomAppbar} />}
       {!!bottomNavigation && !keyboard.shown && (
         <BottomNavigation {...bottomNavigation} />
