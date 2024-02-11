e83c8bcfa5073e9b9a2f72088c71d3e430d044ea
Genius Chinembiri <geniuschinembiri@gmail.com>
2024-02-11 12:42:12 +0200

    fix: remove the bottomsheet black page bug

--- src/components/BottomSheetsProvider.tsx
+++ src/components/BottomSheetsProvider.tsx
@@ -99,11 +99,6 @@ export function BottomSheetsProvider(props: BottomSheetsProviderProps) {
             ? theme.color.Stone[800]
             : theme.color.White,
         }}
-        containerStyle={{
-          backgroundColor: isDarkMode
-            ? theme.color.Stone[800]
-            : theme.color.White,
-        }}
         ref={ref}
         index={-1}
         enablePanDownToClose
